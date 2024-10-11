a169543374989e4d4dec101765842bafdb7413c8
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-10-11 20:55:09 +0200

    feat: add abstraction functions for controller response and error handling

--- backend/family-tree-api/src/family_tree_api/controllers/members_controller.clj
+++ backend/family-tree-api/src/family_tree_api/controllers/members_controller.clj
@@ -1,8 +1,6 @@
 (ns family-tree-api.controllers.members-controller
-  (:use [ring.util.response :only [response content-type status]])
   (:require [family-tree-api.services.members-service :as members-service]
-            [cheshire.core :as json]
-            [family-tree-api.controllers.utils :refer [get-exception-type]]))
+            [family-tree-api.controllers.utils :refer [json-response is-exception-bad-request?]]))
 
 (defn create-member [member]
   ;;   TODO
@@ -14,18 +12,10 @@
 
 (defn get-member-by-id [id]
   (try
-    (-> (response (json/generate-string
-                   (members-service/get-member-by-id id)))
-        (content-type "application/json")
-        (status 200))
+    (json-response {:data (members-service/get-member-by-id id) :status-code 200})
     (catch Exception e
-      (-> (response (json/generate-string
-                     e))
-          (content-type "application/json")
-          (status
-           (if (= "validation-error" (get-exception-type e))
-             400
-             500))))))
+      (json-response {:data (ex-data e)
+                      :status-code (if (is-exception-bad-request? e) 400 500)}))))
 
 (defn search-member-by-name [name]
 ;;   TODO
--- backend/family-tree-api/src/family_tree_api/controllers/utils.clj
+++ backend/family-tree-api/src/family_tree_api/controllers/utils.clj
@@ -1,5 +1,19 @@
 (ns family-tree-api.controllers.utils
-  (:require [family-tree-api.utils.general :refer [nil-safe-pipe]]))
+  (:use [ring.util.response :only [response content-type status]])
+  (:require
+   [cheshire.core :as json]
+   [family-tree-api.utils.general :refer [nil-safe-pipe]]))
+
+(def bad-request-error-types ["validation-error"])
 
 (defn get-exception-type [e]
   (nil-safe-pipe e ex-data :type))
+
+(defn is-exception-bad-request? [e]
+  (.contains bad-request-error-types (get-exception-type e)))
+
+(defn json-response [{:keys [data status-code]}]
+  (-> (response (json/generate-string
+                 data))
+      (content-type "application/json")
+      (status status-code)))
